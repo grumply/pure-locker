7abbb8da3c568ab1f5d54635df4efff643efdfb5
grumply <sean@grump.ly>
2019-05-02 05:42:49 -0500

    Add exceptions to default.nix.

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
-{ mkDerivation, base, containers, stm, stdenv }:
+{ mkDerivation, base, containers, exceptions, stm, mtl, stdenv }:
 mkDerivation {
   pname = "pure-locker";
   version = "0.7.0.0";
   src = ./.;
-  libraryHaskellDepends = [ base containers stm ];
+  libraryHaskellDepends = [ base containers exceptions stm mtl ];
   homepage = "github.com/grumply/pure-locker";
   description = "Shared resource management";
   license = stdenv.lib.licenses.bsd3;
